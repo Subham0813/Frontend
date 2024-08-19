3599e5c1901b9d369c77991efb2b9cc052a8ef98
user.email <subham913dces3@gmail.com>
2024-08-19 23:50:32 +0530

    updated Dom manupulations

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -1,13 +1,11 @@
 const items = document.querySelector(".items");
 const item = document.createElement("div");
-item.add;
-item.className = "item";
+
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) =>
     data.forEach((desert, index) => {
-      console.log(desert);
-      console.log(index);
+      console.log(data);
       let price = desert.price.toFixed(2);
       const item = document.createElement("div");
       item.className = `item`;
@@ -27,12 +25,6 @@ fetch("./data.json")
                 <img src="./assets/images/icon-add-to-cart.svg" alt="icon" />
                 <span>Add to cart</span>
               </div>
-              
-              <div class="button selected-btn" id = ${index}>
-                <img id="add" src="./assets/images/icon-increment-quantity.svg" alt="add" />
-                <span id="count-item">1</span>
-                <img id="remove" src="./assets/images/icon-decrement-quantity.svg" alt="add" />
-              </div>
 
               <div class="details">
                 <p class="category">${desert.category}</p>
@@ -45,7 +37,72 @@ fetch("./data.json")
     })
   );
 
+const body = document.querySelector("body");
 const cartIcon = document.querySelector(".cart-icon");
-cartIcon.addEventListener("click", () => {
-  console.dir(cartIcon);
+const cartContainer = document.querySelector(".cart-container");
+const cartCancelButton = document.querySelector(".cancel-btn");
+
+let counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+body.addEventListener("click", (e) => {
+  const target = e.target;
+  console.dir(target);
+  console.dir(target.parentElement);
+
+  if (target === cartIcon || target.parentElement === cartIcon) {
+    cartContainer.classList.add("show-container");
+  }
+
+  if (
+    target === cartCancelButton ||
+    target.parentElement === cartCancelButton ||
+    target === cartContainer
+  ) {
+    cartContainer.classList.remove("show-container");
+  }
+
+  let addButton;
+
+  if (target.classList.contains("card-button")) {
+    addButton = target;
+  } else if (target.parentElement.classList.contains("card-button"))
+    addButton = target.parentElement;
+
+  if (addButton != null && addButton) {
+    console.dir(target);
+    const id = target.parentElement.id;
+    counts[id]++;
+
+    addButton.innerHTML = `
+        <img id="add" src="./assets/images/icon-increment-quantity.svg" alt="add" />
+        <span id="count-item">${counts[id]}</span>
+        <img id="remove" src="./assets/images/icon-decrement-quantity.svg" alt="add" />
+      `;
+
+    addButton.classList.add("selected-btn");
+    addButton.classList.remove("card-button");
+  }
 });
+
+cartIcon.addEventListener("click", (e) => {
+  console.log(e);
+});
+cartCancelButton.addEventListener("click", (e) => {
+  console.log(e);
+});
+
+// const cardButton = document.querySelector("");
+// const itemsArray = [];
+// cardButton.addEventListener("click", (e) => {
+//   let count = 0;
+//   console.dir(e);
+//   //cardButton.parentNode
+
+//   cardButton.classList.add(selected - btn);
+//   cardButton.classList.remove(card - button);
+//   cardButton.innerHTML = `
+//     <img id="add" src="./assets/images/icon-increment-quantity.svg" alt="add" />
+//     <span id="count-item">${item}</span>
+//     <img id="remove" src="./assets/images/icon-decrement-quantity.svg" alt="add" />
+//   `;
+// });
